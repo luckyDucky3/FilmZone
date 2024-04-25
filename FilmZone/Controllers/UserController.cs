92a16e5f64888fda447dfb0e66205df9c58993e9
Lebedev Maksim <maxlebed04@mail.ru>
2024-04-25 21:14:49 +0300

    develop/добавление форм регистрации и HttpGet и Post методов

--- FilmZone/Controllers/UserController.cs
+++ FilmZone/Controllers/UserController.cs
@@ -14,10 +14,25 @@ namespace FilmZone.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public string Registration(string LastName, string FirstName, string NickName, string Email, string Password1, string Password2)
+        {
+            if (Password1 == Password2)
+                return
+                    $"Your lastname: {LastName}, firstname: {FirstName}, nickname: {NickName}, email: {Email}, password: {Password1}";
+            else
+                return "Пароли не совпадают, повторите попытку";
+        }
         [HttpGet]
         public IActionResult ForgotPassword()
         {
             return View();
+        }
+        [HttpPost]
+        public string ForgotPassword(string Email)
+        {
+            return "Ссылка о смене пароля отправленна вам на почту:)\nЕсли ссылка не приходит, пожалуйста, еще раз проверьте правильность введенной почты или проверьте папку спам";
         }
     }
 }

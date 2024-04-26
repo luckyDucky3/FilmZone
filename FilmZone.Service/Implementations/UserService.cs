9c0e11ebe6d10a681e1f73e1196f0b83d9bfc6cd
Lebedev Maksim <maxlebed04@mail.ru>
2024-04-26 12:09:10 +0300

    develop/добавление логики в service

--- FilmZone.Service/Implementations/UserService.cs
+++ FilmZone.Service/Implementations/UserService.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FilmZone.Domain.Models;
+using FilmZone.Domain.Response;
+using FilmZone.Domain.ViewModels;
+using FilmZone.Service.Interfaces;
 
 namespace FilmZone.Service.Implementations
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public Task<IBaseResponse<IEnumerable<User>>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<User>> GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<User>> CreateUser(FilmViewModel carViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<bool>> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<Film>> GetUserByLastName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<Film>> Edit(int id, User model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- FilmZone.Service/Interfaces/IUserService.cs
+++ FilmZone.Service/Interfaces/IUserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FilmZone.Domain.Models;
+using FilmZone.Domain.Response;
+using FilmZone.Domain.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,16 @@ namespace FilmZone.Service.Interfaces
 {
     public interface IUserService
     {
+        Task<IBaseResponse<IEnumerable<User>>> GetUsers();
+
+        Task<IBaseResponse<User>> GetUser(int id);
+
+        Task<IBaseResponse<User>> CreateUser(FilmViewModel carViewModel);
+
+        Task<IBaseResponse<bool>> DeleteUser(int id);
+
+        Task<IBaseResponse<Film>> GetUserByLastName(string name);
+
+        Task<IBaseResponse<Film>> Edit(int id, User model);
     }
 }

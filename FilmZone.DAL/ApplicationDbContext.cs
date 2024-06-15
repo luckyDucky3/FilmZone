d0f7099c3de5c245447829e3cc80a3c8d77e00e5
Максим Лебедев <Maxlebed04@mail.ru>
2024-06-15 14:41:48 +0300

    develop/test

--- FilmZone.DAL/ApplicationDbContext.cs
+++ FilmZone.DAL/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using FilmZone.DAL.Repositories;
 using FilmZone.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 namespace FilmZone.DAL
 {
     public class ApplicationDbContext : DbContext
@@ -16,20 +17,26 @@ namespace FilmZone.DAL
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            try
-            {
-                optionsBuilder.UseNpgsql(
-                    "Host=localhost;Port=5432;Database=filmdb;Username=postgres;Password=100506Ki");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка подключения к БД: {ex}");
-            }
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //try
+        //    //{
+        //    //    if(optionsBuilder.UseNpgsql(
+        //    //        "Host=localhost;Port=5432;Database=filmdb;Username=postgres;Password=100506Ki").IsConfigured)
+        //    //        Console.WriteLine("Подключение к бд прошло успешно");
+        //    //}
+        //    //catch (Exception ex)
+        //    //{
+        //    //    Console.WriteLine($"Ошибка подключения к БД: {ex}");
+        //    //}
+        //}
         public DbSet<Film> Film { get; set; }
         public DbSet<User> User { get; set; }
+        static void Main() 
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            Console.ReadLine();
+        }
     }
 
 }
